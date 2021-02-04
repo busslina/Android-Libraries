6c0262286ef84e8426cdbfc44faf2aa9d439ee48
busslina <73592852+busslina@users.noreply.github.com>
2021-02-04 20:25:20 +0100

    snapshot

--- main-lib/src/main/java/com/busslina/main_lib/core/modules/ForegroundServiceBase.kt
+++ main-lib/src/main/java/com/busslina/main_lib/core/modules/ForegroundServiceBase.kt
@@ -5,7 +5,24 @@ import com.busslina.main_lib.core.commons.CommonsModules
 
 abstract class ForegroundServiceBase: Service {
 
+    companion object {
+        const val NOTIFICATION_ID = 101
+        const val STATE_STOPPED = 0
+        const val STATE_STARTED = 1
+    }
+
+    var state = STATE_STOPPED
+
+
     constructor(): super() {
         CommonsModules.foregroundService = this
+    }
+
+    fun isStopped(): Boolean {
+        return state == STATE_STOPPED
+    }
+
+    fun isStarted(): Boolean {
+        return state == STATE_STARTED
     }
 }
