40eb83a9299489f4a78539e1999680dcda063016
busslina <73592852+busslina@users.noreply.github.com>
2021-02-05 19:42:17 +0100

    snapshot

--- main-lib/src/main/java/com/busslina/main_lib/core/interfaces/MainActivityI.kt
+++ main-lib/src/main/java/com/busslina/main_lib/core/interfaces/MainActivityI.kt
@@ -11,7 +11,7 @@ interface MainActivityI {
     var foregroundServiceIntent: Intent
 
     fun initMethodChannel()
-    fun sendMessageMethodChannel()
+    fun sendMessageMethodChannel(method: String, args: Any?)
 
     fun startForegroundService()
     fun stopForegroundService()
