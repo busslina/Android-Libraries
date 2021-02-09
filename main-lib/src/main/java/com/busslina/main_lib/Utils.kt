b5a18d97c379a24fcadd161cf3dca0902fe34963
busslina <73592852+busslina@users.noreply.github.com>
2021-02-09 02:17:33 +0100

    snapshot

--- main-lib/src/main/java/com/busslina/main_lib/Utils.kt
+++ main-lib/src/main/java/com/busslina/main_lib/Utils.kt
@@ -1,5 +1,34 @@
 package com.busslina.main_lib
 
+import java.util.*
+
 class Utils {
+
+    companion object {
+
+        ///
+        fun getElapsedTime(initialDate: Date): String {
+//            val diff = Date().time - initialDate.time
+//            val secondsTotal = (diff / 1000)
+            val secondsTotal = getDateDiff(initialDate)
+            val seconds = secondsTotal % 60
+            val minutesTotal = (secondsTotal / 60)
+            val minutes = minutesTotal % 60
+            val hours = minutesTotal / 60
+            val days = hours / 24
+
+            val secondsF = seconds.toString().padStart(2, '0')
+            val minutesF = minutes.toString().padStart(2, '0')
+            val hoursF = hours.toString().padStart(2, '0')
+
+            return "$hoursF:$minutesF:$secondsF"
+        }
+
+        /// Difference between two dates in seconds.
+        fun getDateDiff(initialDate: Date, finalDate: Date = Date()): Int {
+            val diff = finalDate.time - initialDate.time
+            return (diff / 1000).toInt()
+        }
+    }
 
 }
