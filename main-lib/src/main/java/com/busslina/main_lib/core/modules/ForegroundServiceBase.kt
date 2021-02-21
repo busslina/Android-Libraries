ccabb6fec2cb6e1c6f8ac85801bc5bf0273a9596
busslina <73592852+busslina@users.noreply.github.com>
2021-02-21 20:20:25 +0100

    snapshot

--- main-lib/src/main/java/com/busslina/main_lib/core/modules/ForegroundServiceBase.kt
+++ main-lib/src/main/java/com/busslina/main_lib/core/modules/ForegroundServiceBase.kt
@@ -259,12 +259,12 @@ abstract class ForegroundServiceBase: Service {
     /**
      * 01 - Get notification.
      */
-    abstract fun getNotification(text: String = notificationText, actionName: String? = null, actionValue: Any? = null): Notification
+    abstract fun getNotification(text: String? = notificationText, actionName: String? = null, actionValue: Any? = null): Notification
 
     /**
      * 02 - Update notification.
      */
-    abstract fun updateNotification(text: String = notificationText)
+    abstract fun updateNotification(text: String? = notificationText)
 
     /**
      * 03- Create websocket sub-module.
